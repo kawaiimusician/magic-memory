0a622a108085238c8a02494fcfd47bfddc75ddc4
Rebecca <rebecca@heyman.com>
2022-02-23 22:41:28 -0600

    upload magic memory game

---
+++ src/components/SingleCard.js
@@ -0,0 +1,27 @@
+import './SingleCard.css'
+
+function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+  };
+
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} className='front' alt="card front" />
+        <img 
+          src="/img/cover.png" 
+          alt="card cover" 
+          className='back' 
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default SingleCard;
